d77b58a11e7b0a1374df23cfc1f5eb4897695010
Kribl, Dennis <d.kribl@immowelt.de>
2020-05-22 15:14:14 +0200

    minor refactoring
    Signed-off-by: Kribl, Dennis <d.kribl@immowelt.de>

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -6,6 +6,7 @@ const Footer = props => {
       <div className="row">
         <div className="column">
           <p>Made with ♥ by Dennis Kribl</p>
+          <p className="plug">Find the website source code on <a href="https://github.com/denniskribl/denniskribl.com">GitHub</a></p>
         </div>
       </div>
     </div>
---
+++ src/pages/404.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+import Layout from "../components/layout"
+
+export default props => {
+  return (
+    <Layout pageTitle="How did I get here?" pageDescription="">
+      <p>How did you get there?</p>
+      <p><a href="https://www.youtube.com/watch?v=zEAgCDIRqo4">Just stay away from the coffee machine...</a></p>
+    </Layout>
+  )
+}
