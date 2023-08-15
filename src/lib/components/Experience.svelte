7eb6601fe72c01acc0c0bbc9b3d716d510a2b9c7
denniskribl <denniskribl@gmail.com>
2023-08-15 10:08:51 +0200

    chore: simplify code
    Signed-off-by: denniskribl <denniskribl@gmail.com>

--- src/lib/components/Experience.svelte
+++ src/lib/components/Experience.svelte
@@ -1,25 +1,44 @@
+<script lang="ts">
+	interface Experience {
+		title: string;
+		at: string;
+		duration: string;
+		description?: string;
+	}
+
+	const experience: Experience[] = [
+		{
+			title: "Cloud Site Reliability Engineer",
+			at: "???",
+			duration: "2023 - present"
+		},
+		{
+			title: "DevOps Engineer / Full Stack Engineer",
+			at: "immowelt / AVIV Group",
+			duration: "2018 - 2023",
+		},
+		{
+			title: "Linux System Administrator",
+			at: "immowelt",
+			duration: "2017 - 2018",
+		},
+		{
+			title: "IT specialist/systems integration apprentice",
+			at: "immowelt",
+			duration: "2014 - 2017",
+		}
+	]
+</script>
+
 <div class="w-full">
 	<h1 class="lg:text-5xl text-2xl font-bold">Experience</h1>
 	<ul class="steps steps-vertical py-6">
-		<li data-content="●" class="step step-secondary">
-			<p class="lg:text-2xl text-xl py-2 lg:py-6 text-left">
-				Cloud Site Reliability Engineer @ ??? (2023 - present)
-			</p>
-		</li>
-		<li data-content="●" class="step step-secondary">
-			<p class="lg:text-2xl text-xl py-2 lg:py-6 text-left">
-				DevOps Engineer @ immowelt / AVIV Group (2018 - 2023)
-			</p>
-		</li>
-		<li data-content="●" class="step step-secondary">
-			<p class="lg:text-2xl text-xl py-2 lg:py-6 text-left">
-				Linux System Administrator @ immowelt (2017 - 2018)
-			</p>
-		</li>
-		<li data-content="●" class="step step-secondary">
-			<p class="lg:text-2xl text-xl py-2 lg:py-6 text-left">
-				IT Specialist/systems integration apprentice @ immowelt (2014 - 2017)
-			</p>
-		</li>
+		{#each experience as exp}
+			<li data-content="●" class="step step-secondary">
+				<p class="lg:text-2xl text-xl py-2 lg:py-6 text-left">
+					{exp.title} @ {exp.at} ({exp.duration})
+				</p>
+			</li>
+		{/each}
 	</ul>
 </div>
