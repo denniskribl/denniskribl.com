4d285140ea860d99ba4fe38b6712a8a55e6eb482
denniskribl <denniskribl@gmail.com>
2023-08-17 12:13:16 +0200

    fix: lint stage config
    Signed-off-by: denniskribl <denniskribl@gmail.com>

--- lint-staged.config.cjs
+++ lint-staged.config.cjs
@@ -1,3 +1,4 @@
 module.exports = {
-	'*.{js,ts,svelte,css,scss,postcss,md,json}': ['yarn format']
+	'*.{js,ts,svelte,css,scss,postcss,md,json}': ['yarn format'],
+	'*.{js,ts,svelte}': 'eslint .'
 };
