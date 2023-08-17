55470d9aef9b3e1db1bb0c29deeab1fe8c9e1a70
denniskribl <denniskribl@gmail.com>
2023-08-17 10:17:57 +0200

    feat: add pr envs
    Signed-off-by: denniskribl <denniskribl@gmail.com>

--- sst.config.ts
+++ sst.config.ts
@@ -12,11 +12,12 @@ export default {
 		app.stack(function Site({ stack }) {
 			const site = new SvelteKitSite(stack, 'site', {
 				customDomain: {
-					domainName: 'denniskribl.com'
+					hostedZone: 'denniskribl.com',
+					domainName: stack.stage === 'prod' ? 'denniskribl.com' : `${stack.stage}.denniskribl.com`
 				}
 			});
 			stack.addOutputs({
-				url: site.url
+				url: site.customDomainUrl ?? site.url
 			});
 		});
 	}
