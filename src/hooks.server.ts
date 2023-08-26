98b3aa92a14e8a92f718d0ebc73668855f9f0d1c
denniskribl <denniskribl@gmail.com>
2023-08-26 04:07:08 +0200

    feat: add sentry
    Signed-off-by: denniskribl <denniskribl@gmail.com>

---
+++ src/hooks.server.ts
@@ -0,0 +1,20 @@
+import { sequence } from '@sveltejs/kit/hooks';
+import { handleErrorWithSentry, sentryHandle } from '@sentry/sveltekit';
+import * as Sentry from '@sentry/sveltekit';
+import { ProfilingIntegration } from '@sentry/profiling-node';
+
+Sentry.init({
+	dsn: 'https://891e1a700d440672bd8be4d43cc16368@o4505768619212800.ingest.sentry.io/4505768632057856',
+	tracesSampleRate: 1.0,
+	profilesSampleRate: 1.0, // Profiling sample rate is relative to tracesSampleRate
+	integrations: [
+		// Add profiling integration to list of integrations
+		new ProfilingIntegration()
+	]
+});
+
+// If you have custom handlers, make sure to place them after `sentryHandle()` in the `sequence` function.
+export const handle = sequence(sentryHandle());
+
+// If you have a custom error handler, pass it to `handleErrorWithSentry`
+export const handleError = handleErrorWithSentry();
