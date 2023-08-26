001b1a2d365cd00353be24388bce1bae553cbeb5
denniskribl <denniskribl@gmail.com>
2023-08-26 04:35:19 +0200

    Revert "feat: add sentry"
    This reverts commit 98b3aa92a14e8a92f718d0ebc73668855f9f0d1c.

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,7 @@
-import { sentrySvelteKit } from '@sentry/sveltekit';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 import { imagetools } from 'vite-imagetools';
 
 export default defineConfig({
-	plugins: [
-		sentrySvelteKit({
-			sourceMapsUploadOptions: {
-				org: 'denniskribl',
-				project: 'denniskriblcom'
-			}
-		}),
-		sveltekit(),
-		imagetools()
-	]
+	plugins: [sveltekit(), imagetools()]
 });
