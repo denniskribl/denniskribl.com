991e0de43583c1de6284f80827539afd6bef5144
denniskribl <denniskribl@gmail.com>
2023-08-26 04:19:28 +0200

    fix: remove profiler
    Signed-off-by: denniskribl <denniskribl@gmail.com>

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -5,12 +5,7 @@ import { ProfilingIntegration } from '@sentry/profiling-node';
 
 Sentry.init({
 	dsn: 'https://891e1a700d440672bd8be4d43cc16368@o4505768619212800.ingest.sentry.io/4505768632057856',
-	tracesSampleRate: 1.0,
-	profilesSampleRate: 1.0, // Profiling sample rate is relative to tracesSampleRate
-	integrations: [
-		// Add profiling integration to list of integrations
-		new ProfilingIntegration()
-	]
+	tracesSampleRate: 1.0
 });
 
 // If you have custom handlers, make sure to place them after `sentryHandle()` in the `sequence` function.
